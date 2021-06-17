85f24b6eea1d9f2b3afcc55b37b31430cd74a03b
johstill <79176865+johstill@users.noreply.github.com>
2021-06-17 06:11:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function startLogic() {
+    var newHtml = '<em>New Header</em>';
+    document.getElementById('headerItem').innerHTML = newHtml;
+}
+taillwing.config.js
+module.exports={
+  variants:{
+    extend:{
+      ...
+      boxshadow:['active']
+    }
+  }
+}
